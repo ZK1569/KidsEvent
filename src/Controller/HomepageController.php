8aaeb663748874e3e0ae318c071e739515cb14ab
ZK1569 <cristian.ursu.2001@gmail.com>
2022-03-17 16:02:45 +0100

    new project

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+
+    public function __construct(private ProductRepository $productRepository)
+    {
+        $this->productRepository->findAll();
+    }
+
+    #[Route('/', name:'homepage.index')]
+    public function index():Response{
+
+        return $this->render('homepage/index.html.twig', [
+            "contenu" => $this->productRepository->findAll(),
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $price;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $slug;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $main_picture;
+
+    #[ORM\Column(type: 'text')]
+    private $short_description;
+
+    #[ORM\Column(type: 'integer')]
+    private $time;
+
+    #[ORM\Column(type: 'integer')]
+    private $age_min;
+
+    #[ORM\Column(type: 'integer')]
+    private $age_max;
+
+    #[ORM\Column(type: 'integer')]
+    private $min_kids;
+
+    #[ORM\Column(type: 'integer')]
+    private $max_kids;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getMainPicture(): ?string
+    {
+        return $this->main_picture;
+    }
+
+    public function setMainPicture(string $main_picture): self
+    {
+        $this->main_picture = $main_picture;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->short_description;
+    }
+
+    public function setShortDescription(string $short_description): self
+    {
+        $this->short_description = $short_description;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getAgeMin(): ?int
+    {
+        return $this->age_min;
+    }
+
+    public function setAgeMin(int $age_min): self
+    {
+        $this->age_min = $age_min;
+
+        return $this;
+    }
+
+    public function getAgeMax(): ?int
+    {
+        return $this->age_max;
+    }
+
+    public function setAgeMax(int $age_max): self
+    {
+        $this->age_max = $age_max;
+
+        return $this;
+    }
+
+    public function getMinKids(): ?int
+    {
+        return $this->min_kids;
+    }
+
+    public function setMinKids(int $min_kids): self
+    {
+        $this->min_kids = $min_kids;
+
+        return $this;
+    }
+
+    public function getMaxKids(): ?int
+    {
+        return $this->max_kids;
+    }
+
+    public function setMaxKids(int $max_kids): self
+    {
+        $this->max_kids = $max_kids;
+
+        return $this;
+    }
+}
